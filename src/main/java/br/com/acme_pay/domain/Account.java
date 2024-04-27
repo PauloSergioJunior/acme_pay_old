8d78bc409fa35e9b83cc3771d7338ef186bf75d8
PauloSergioJunior <juniorvasco616@gmail.com>
2024-04-27 10:30:08 -0300

    Adding Methods in Account

--- src/main/java/br/com/acme_pay/domain/Account.java
+++ src/main/java/br/com/acme_pay/domain/Account.java
@@ -1,6 +1,7 @@
 package br.com.acme_pay.domain;
 
 import br.com.acme_pay.enums.AccountType;
+import br.com.acme_pay.exception.BalanceToWithdrawException;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,6 +13,29 @@ public class Account {
     private Integer agencyNumber;
     private BigDecimal balance;
     private Enum<AccountType> accountType;
+
+    public void deposit(BigDecimal amount){
+        this.balance.add(amount);
+    }
+
+    public void withdraw(BigDecimal amount) throws BalanceToWithdrawException{
+
+        checkBalance(amount);
+        this.balance.subtract(amount);
+
+    }
+
+    public void transfer(Account targetAccount, BigDecimal valueTranfer) throws BalanceToWithdrawException {
+        this.withdraw(valueTranfer);
+        targetAccount.deposit(valueTranfer);
+    }
+
+    private void checkBalance(BigDecimal amount) throws BalanceToWithdrawException {
+        if (this.balance.compareTo(amount) < 0){
+            throw new BalanceToWithdrawException("error withdraw");
+        }
+
+    }
 
 
 }
---
+++ src/main/java/br/com/acme_pay/exception/BalanceToWithdrawException.java
@@ -0,0 +1,8 @@
+package br.com.acme_pay.exception;
+
+public class BalanceToWithdrawException extends Exception{
+
+    public BalanceToWithdrawException(String menssage){
+        super(menssage);
+    }
+}
